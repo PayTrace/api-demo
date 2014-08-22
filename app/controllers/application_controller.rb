a5ef05bcf019eca181f813bd85de7d41f6efbd66
Richard Weeks <rtweeks21@gmail.com>
2014-08-22 13:07:13 -0700

    Pointed at production server and moved the PayTrace token acquisition to ApplicationController.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  def self.paytrace_connection
+    OAuth2::Client.new(
+      nil, nil, :site => 'https://api.paytrace.com'
+    ).password.get_token(
+      *Rails.configuration.paytrace_api_credentials
+    )
+  end
+  
+  protected
+  def paytrace_connection
+    self.class.paytrace_connection
+  end
 end
