02d487344a28fe565ff250c6de03802cee0a08ac
Tom Canham <tom@paytrace.com>
2014-10-07 10:20:35 -0700

    Finish up the PHP demo. I feel dirty.

--- php/keyed-sale-transaction/hermes_demo.php
+++ php/keyed-sale-transaction/hermes_demo.php
@@ -1,17 +1,46 @@
 <?php
 
+// use the Guzzle HTTP client library
+// see: http://guzzle.readthedocs.org/en/latest/index.html
 require 'vendor/autoload.php';
-
 $client = new GuzzleHttp\Client();
-$res = $client->post('http://localhost:3000/oauth/token', [
+
+// DRY
+$auth_server = 'http://localhost:3000';
+
+// send a request to the authentication server
+// note: normally, you'd store the username/password in a more secure fashion!
+$res = $client->post("$auth_server/oauth/token", [
   'body' => [
     'grant_type' => 'password', 
     'username' => 'demo123', 
     'password' => 'demo123'
     ]
   ]);
+
+// grab the results (as JSON)
 $json = $res->json();
-// var_export($json);             // Outputs the JSON decoded data
+
+// get the access token
 $token = $json['access_token'];
+
+// create a fake sale transaction
+$sale_data = [
+  'amount' => 19.99,
+  'credit_card' => [
+    'number' => '4111111111111111',
+    'expiration_month' => 12,
+    'expiration_year' => 20
+  ]
+];
+
+// post the transaction to hermes
+$res = $client->post("$auth_server/v1/transactions/sale/keyed", [
+  'headers' => ['Authorization' => "Bearer $token"],
+  'json' => $sale_data
+  ]);
+
+// dump the json results
+var_export($res->json());
 
 ?>
